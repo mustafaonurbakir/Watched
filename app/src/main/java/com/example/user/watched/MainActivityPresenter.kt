aa0435ca07323e97b12c705b2935fbfbeb01182e
mustafaonurbakir <bakirmus@itu.edu.tr>
2020-01-27 22:28:47 +0300

    aa

--- app/src/main/java/com/example/user/watched/MainActivityPresenter.kt
+++ app/src/main/java/com/example/user/watched/MainActivityPresenter.kt
@@ -3,7 +3,15 @@ package com.example.user.watched
 class MainActivityPresenter {
 
 
-
     fun MainActivityPresenter(){
+    }
+
+    fun solve(value: MovieTypes): MovieTypes{
+        if(value == MovieTypes.Action){
+            return MovieTypes.Action
+        }
+        else{
+            return MovieTypes.Horror
+        }
     }
 }
