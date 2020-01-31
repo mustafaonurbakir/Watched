7eed928d75ee31ab28c07a3dc8831384e893da0f
mustafaonurbakir <mustafaonurbakir@gmail.com>
2020-01-31 23:48:54 +0300

    minor changes

--- app/src/main/java/com/example/user/watched/database/dao/MoviesDAO.kt
+++ app/src/main/java/com/example/user/watched/database/dao/MoviesDAO.kt
@@ -1,12 +1,12 @@
-package com.example.kolindeneme.database.dao
+package com.example.user.watched.database.dao
 
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
-import com.example.kolindeneme.database.entities.MoviesEntity
-import com.example.kolindeneme.database.entities.MoviesEntity.Companion.TABLE_NAME
+import com.example.user.watched.database.entities.MoviesEntity
+import com.example.user.watched.database.entities.MoviesEntity.Companion.TABLE_NAME
 import io.reactivex.Flowable
 import io.reactivex.Single
 
