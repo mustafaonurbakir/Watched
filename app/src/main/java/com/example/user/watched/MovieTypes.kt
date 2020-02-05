1b2c82992e5051f258549138c0b11d4c1327534b
mustafaonurbakir <bakirmus@itu.edu.tr>
2020-02-05 23:53:33 +0300

    new file

--- app/src/main/java/com/example/user/watched/MovieTypes.kt
+++ app/src/main/java/com/example/user/watched/MovieTypes.kt
@@ -2,5 +2,20 @@ package com.example.user.watched
 
 enum class MovieTypes{
     Action,
-    Horror
+    Horror,
+    Drama,
+    Fantasy,
+    Historical,
+    Mystery,
+    Political,
+    Romance,
+    Science_Fiction,
+    Thriller,
+    Social,
+    Speculative,
+    Western,
+    Animation,
+    Adventure,
+    Comedy,
+    Crime
 }
